6cb3c403354a4d59269e300e2ad1115690a09bf0
carolmedici <carolina.obstetriz@gmail.com>
2023-04-09 20:16:43 -0300

    :test_tube: footer

---
+++ src/Components/Footer1/index.jsx
@@ -0,0 +1,10 @@
+
+const Footer1 = () => {
+    return(
+        <footer>
+            <p> design by <a href="http://github.com/carolmedici">Carol Médici</a></p>
+        </footer>
+    )
+}
+
+export default Footer1
--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Components/Header";
 import Cards from "../Components/Countries/Cards";
+import Footer1 from "../Components/Footer1";
 
 
 const HomePage = () => {
@@ -12,6 +13,7 @@ const HomePage = () => {
     <>
       <Header />
       <Cards />
+      <Footer1 />
     </>
   );
 };
