ec291e1207f218ca25945647d8cb486e8df78136
carolmedici <carolina.obstetriz@gmail.com>
2023-04-08 22:10:29 -0300

    lipstick: improving layout cards

--- src/Components/Countries/Cards/index.jsx
+++ src/Components/Countries/Cards/index.jsx
@@ -1,9 +1,6 @@
-import React, {useEffect, useState} from "react";
-import api from '../../services/api';
-
-
-
-
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import style from "./Cards.module.scss";
 
 const Cards = () => {
   const [cards, setCards] = useState([]);
@@ -20,15 +17,19 @@ const Cards = () => {
   }, []);
 
   return (
-    <div >
-      {cards.map(card => (
-        <li key={card.id}>
-          Flag: {card.flag} <br/>
-          Name: {card.name.common} <br/>
-          Language: {card.languages} <br/>
-          Capital: {card.capital}
-        </li>
-      ))}
+
+    <div className={style.container}>
+      <div className={style.countries__cards}>
+        {cards.map(card => (
+          <li key={card.name.common} className={style.countries__card}>
+            <img src={card.flags.png} alt={card.flags.alt} className={style.countries__img}/>
+            <p className={style.countries__name}> {card.name.common}</p>
+            <p className={style.countries__data}><strong>Language:</strong> {Object.keys(card.languages).map((key) => card.languages[key]).join(', ')} </p>
+            <p className={style.countries__data}><strong>Capital:</strong> {card.capital}</p>
+            <p className={style.countries__data}><strong>Region: </strong>{card.region}</p>
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
--- src/Components/services/api.js
+++ src/Components/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://restcountries.com/v3.1/all?fields=name,flag,language,capital",
+  baseURL: "https://restcountries.com/v3.1/all?fields=name,flags,languages,capital,region",
 });
 
 export default api;
